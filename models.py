9ba944653df3cd33e6924729113560ab416dd9ec
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-09-18 20:19:43 +0500

    Redefine project

---
+++ models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime)
+    status = Column(String)
+
+    items = relationship("OrderItem", back_populates="order")
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+    order = relationship("Order", back_populates="items")
+    product = relationship("Product")
---
+++ schemas.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+class ProductCreate(ProductBase):
+    pass
+
+class Product(ProductBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class OrderItemBase(BaseModel):
+    product_id: int
+    quantity: int
+
+class OrderItemCreate(OrderItemBase):
+    pass
+
+class OrderItem(OrderItemBase):
+    id: int
+    order_id: int
+
+    class Config:
+        from_attributes = True
+
+class OrderBase(BaseModel):
+    status: str
+
+class OrderCreate(OrderBase):
+    items: List[OrderItemCreate]
+
+class Order(OrderBase):
+    id: int
+    created_at: datetime
+    items: List[OrderItem]
+
+    class Config:
+        from_attributes = True
